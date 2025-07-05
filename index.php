472966c6695c879fe46d8a98995bfb9bf582b80d
Yuda Kurnia Nurul Fikri <yudakurnia.nurulfikri@gmail.com>
2025-07-05 15:45:12 +0700

    a message in index.php

--- index.php
+++ index.php
@@ -10,3 +10,13 @@
     <?php include 'view/footer.php';?>
   </body>
 </html>
+
+<!--
+i am really happy that finally this code is submitted. but i hope it won't
+happened again. frustated by the perfectionist mode, i barely haunted to post-
+pone the submission until now. seems a bad practice for me, but i made myself
+sure that i wont do it on the next semester. My apologize that i could not
+response your whatsapp
+
+:(
+ -->
